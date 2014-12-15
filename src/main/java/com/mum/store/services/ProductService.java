a9205c2007e0e19e11ae5f83e17a49ba365687f5
onlyprithvi <onlyprithvi@gmail.com>
2014-12-15 11:38:06 -0600

    ProductService interface

---
+++ src/main/java/com/mum/store/services/ProductService.java
@@ -0,0 +1,17 @@
+package com.mum.store.services;
+
+import java.util.List;
+
+import com.mum.store.domain.Product;
+
+public interface ProductService {
+	public List<Product> viewAllActiveProducts();
+
+	public Product viewProductById(String id);
+
+	public List<Product> viewAllProducts();
+
+	public List<Product> viewProductIdByTag();
+
+	public void addProducts(Product p);
+}
