85b52465edd12c46e327ca248f946fd482a3d024
onlyprithvi <onlyprithvi@gmail.com>
2014-12-16 18:27:47 -0600

    Prodetail repository

--- src/main/java/com/mum/store/repositories/ProductDetailRepository.java
+++ src/main/java/com/mum/store/repositories/ProductDetailRepository.java
@@ -1,11 +1,12 @@
 package com.mum.store.repositories;
 
 import com.mum.store.domain.Product;
+import com.mum.store.domain.ProductDetails;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductDetailRepository extends CrudRepository<Product, Long> {
+public interface ProductDetailRepository extends CrudRepository<ProductDetails, Long> {
 
 	Product findById(long id);
 
